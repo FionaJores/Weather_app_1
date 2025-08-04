18c996051fd806ddfb39859ac9401c27a4bcd7cc
FionaJores <fionajores10@gmail.com>
2025-08-04 23:02:57 +0530

    Initial commit

---
+++ Weather.js
@@ -0,0 +1,43 @@
+const apiKey="cf1d7f2b020af384183b923fbb131d32";
+
+const apiUrl="https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+
+const searchBox = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search button");
+const weatherIcon = document.querySelector(".weather-icon");
+
+async function checkWeather(city){
+    const response = await fetch(apiUrl+city+`&appid=${apiKey}`);
+
+    var data = await response.json();
+
+    //console.log("Hi..");
+    console.log(data);
+    document.querySelector(".city").innerHTML=data.name;
+    document.querySelector(".temp").innerHTML=Math.round(data.main.temp)+"°C";
+    document.querySelector(".humidity").innerHTML=data.main.humidity+"%";
+    document.querySelector(".wind").innerHTML=data.wind.speed+" km/h";
+
+    if(data.weather[0].main=="Clouds"){
+        weatherIcon.src="images/clouds.png"
+    
+    }
+    else if(data.weather[0].main=="Clear"){
+        weatherIcon.src="images/clear.png";
+    }
+    else if(data.weather[0].main=="Rain"){
+        weather.src="images/rain.png";
+    }
+    else if(data.weather[0].main=="Dizzle"){
+        weatherIcon.src="images/drizzle.png";
+    }
+    else if(data.weather[0].main=="Mist"){
+        weatherIcon.src="images/misy.png";
+    }
+
+}
+
+
+searchBtn.addEventListener("click",()=>{
+      checkWeather(searchBox.value);
+})
